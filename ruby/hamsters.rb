6d3de5d907c93f4d4391a196e3207cfff7bb935b
Pat Holland <patrick.holland3576@gmail.com>
2016-10-25 10:14:34 -0700

    write initial hamster ID program

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is the hamster's name?"
+hamster_name = gets.chomp
+
+puts "How loud is the hamster? (1-10)"
+volume_level = gets.chomp
+
+puts "What color is its fur?"
+fur_color = gets.chomp
+
+puts "Is it a good candidate for adoption? (y/n)"
+good_candidate = gets.chomp
+
+estimated_age = nil
+puts "What is its estimated age?"
+estimated_age = gets.chomp
