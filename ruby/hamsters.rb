4dc73e5b7f2050032dadaa2167204c95f5aec211
Pat Holland <patrick.holland3576@gmail.com>
2016-10-25 10:25:05 -0700

    update program to print to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,6 +3,7 @@ hamster_name = gets.chomp
 
 puts "How loud is the hamster? (1-10)"
 volume_level = gets.chomp
+volume_level.to_i
 
 puts "What color is its fur?"
 fur_color = gets.chomp
@@ -13,3 +14,24 @@ good_candidate = gets.chomp
 estimated_age = nil
 puts "What is its estimated age?"
 estimated_age = gets.chomp
+
+if estimated_age != nil
+  estimated_age.to_i
+end
+
+puts ''
+puts "Your hamster's name is " + hamster_name + "."
+puts hamster_name + " is " + volume_level + " on the loudness scale."
+puts hamster_name + " is " + fur_color + "."
+
+if good_candidate == "y"
+  puts hamster_name + " is a good candidate for adoption."
+else
+  puts hamster_name + " is not a good candidate for adoption."
+end
+
+if estimated_age != nil
+  puts hamster_name + " is about " + estimated_age + " years old."
+else
+  puts "We don't know how old your hamster is."
+end
