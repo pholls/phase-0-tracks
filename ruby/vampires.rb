d00dfe3f205e54a6675f269f881f0acaad077011
Pat Holland <patrick.holland3576@gmail.com>
2016-10-26 14:27:11 -0700

    add until loop to repeat for multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,51 +1,61 @@
-puts "What is your name?"
-employee_name = gets.chomp
+puts "How many employees will be processed?"
+employee_number = gets.chomp
 
-puts "How old are you?"
-age = gets.chomp
+number = 0
+until employee_number.to_i == number do
+  
+  puts "What is your name?"
+  employee_name = gets.chomp
+  
+  puts "How old are you?"
+  age = gets.chomp
+  
+  puts "What year were you born?"
+  birth_year = gets.chomp
+  
+  if ((2016 - birth_year.to_i) == age.to_i) || ((2016 - birth_year.to_i) == (age.to_i + 1))
+    valid_age = true
+  else
+    valid_age = false
+  end
+  
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  garlic_bread = gets.chomp
+  
+  if garlic_bread.downcase == "yes" || garlic_bread.downcase == "y"
+    garlic_bread = true
+  else
+    garlic_bread = false
+  end
+  
+  puts "Would you like to enroll in the company’s health insurance? (y/n)"
+  insurance = gets.chomp
+  
+  if insurance.downcase == "yes" || insurance.downcase == "y"
+    insurance = true
+  else
+    insurance = false
+  end
+  
+  result = "Results inconclusive."
+  if valid_age && (insurance || garlic_bread)
+    result = "Probably not a vampire."
+  end
+  
+  if valid_age == false && (!garlic_bread || !insurance)
+    result = "Probably a vampire."
+  end
+  
+  if valid_age == false && (!garlic_bread && !insurance)
+    result = "Almost certainly a vampire."
+  end
+  
+  if (employee_name.downcase == "drake cula") || (employee_name.downcase == "tu fang")
+    result = "Definitely a vampire."
+  end
+      
+  puts "Results for " + employee_name + ": " + result
+  puts ''
 
-puts "What year were you born?"
-birth_year = gets.chomp
-
-if ((2016 - birth_year.to_i) == age.to_i) || ((2016 - birth_year.to_i) == (age.to_i + 1))
-  valid_age = true
-else
-  valid_age = false
+  number += 1;
 end
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic_bread = gets.chomp
-
-if garlic_bread.downcase == "yes" || garlic_bread.downcase == "y"
-  garlic_bread = true
-else
-  garlic_bread = false
-end
-
-puts "Would you like to enroll in the company’s health insurance? (y/n)"
-insurance = gets.chomp
-
-if insurance.downcase == "yes" || insurance.downcase == "y"
-  insurance = true
-else
-  insurance = false
-end
-
-result = "Results inconclusive."
-if valid_age && (insurance || garlic_bread)
-  result = "Probably not a vampire."
-end
-
-if valid_age == false && (!garlic_bread || !insurance)
-  result = "Probably a vampire."
-end
-
-if valid_age == false && (!garlic_bread && !insurance)
-  result = "Almost certainly a vampire."
-end
-
-if (employee_name.downcase == "drake cula") || (employee_name.downcase == "tu fang")
-  result = "Definitely a vampire."
-end
-    
-puts result
