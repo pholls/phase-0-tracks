f832da406427ccfb482a540a628074da332dd081
Pat Holland <patrick.holland3576@gmail.com>
2016-10-26 12:55:06 -0700

    add employee onboard questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "When were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
+insurance = gets.chomp
