c8f393f7fa86d969b05796264ccf60e1ef54cd37
Pat Holland <patrick.holland3576@gmail.com>
2016-10-26 13:03:29 -0700

    add if/elsif loops for yes and no input

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,10 +1,10 @@
 puts "What is your name?"
-name = gets.chomp
+employee_name = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
 
-puts "When were you born?"
+puts "What year were you born?"
 birth_year = gets.chomp
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
@@ -12,3 +12,23 @@ garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company’s health insurance? (y/n)"
 insurance = gets.chomp
+
+if insurance == "yes"
+  insurance = "y"
+elsif insurance == "no"
+  insurance = "n"
+elsif insurance == "Yes"
+  insurance = "y"
+elsif insurance == "No"
+  insurance = "n"
+end
+
+if garlic_bread == "yes"
+  garlic_bread = "y"
+elsif garlic_bread == "no"
+  garlic_bread = "n"
+elsif garlic_bread == "Yes"
+  garlic_bread = "y"
+elsif garlic_bread == "No"
+  garlic_bread = "n"
+end
