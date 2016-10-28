9421edeadebc4341c8e0c975d228fb91a47782e9
Pat Holland <patrick.holland3576@gmail.com>
2016-10-28 10:54:20 -0700

    add pseudocode for encrypt method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,6 @@
+# Encrypt (take a string as an argument):
+# Break into individual letters
+# Advance each letter one letter forward
+# Loop until the whole string has been advanced one letter forward
+# end
+
