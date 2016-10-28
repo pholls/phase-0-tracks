eb6592be34a5b45b4128e65e86efafa745e785a0
Pat Holland <patrick.holland3576@gmail.com>
2016-10-28 11:23:35 -0700

    write encypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -4,8 +4,25 @@
 # Loop until the whole string has been advanced one letter forward
 # end
 
+def encrypt(password)
+  number = 0
+  encrypted_password = ""
+  until number == password.length
+    if password[number] == " "
+      encrypted_password += " "
+      number += 1
+    end
+    encrypted_password += password[number].next
+    number += 1
+  end
+  puts encrypted_password
+end
+
+encrypt("pass word")
+
 # Decrypt (takes encrypted string as an argument):
 # Break the string into individual letters
 # Reverse each letter one letter backward
 # Loop until the whole string has been reversed one letter back
 # end
+
