5db3c391defad3bba3615d38be928889ca3e9c22
Pat Holland <patrick.holland3576@gmail.com>
2016-10-28 10:58:37 -0700

    add pseudocode for decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,6 +1,11 @@
 # Encrypt (take a string as an argument):
-# Break into individual letters
+# Break the string into individual letters
 # Advance each letter one letter forward
 # Loop until the whole string has been advanced one letter forward
 # end
 
+# Decrypt (takes encrypted string as an argument):
+# Break the string into individual letters
+# Reverse each letter one letter backward
+# Loop until the whole string has been reversed one letter back
+# end
