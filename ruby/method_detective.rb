60d6594903ab72d56c5c09445182ebb7c0aa870a
Pat Holland <patrick.holland3576@gmail.com>
2016-10-31 14:36:08 -0700

    add methods and calls for all examples)

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -11,6 +11,7 @@
 # the receiver in place: that is, it overwrites the input with the output.
 
 "zom".gsub("o", "oo")
+"zom".sub("o", "oo")
 # => “zoom”
 
 "enhance".center(15)
@@ -37,15 +38,21 @@
 # .chop! returns the same result, but the bang means it modifies 
 # the receiver in place: that is, it overwrites the input with the output.
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(1..-1)
+"The mystery of the missing first letter".gsub("T", "")
+"The mystery of the missing first letter".sub("T", "")
+"The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
 "Elementary,    my   dear        Watson!".squeeze
+"Elementary,    my   dear        Watson!".squeeze!
+# .squeeze! returns the same result, but the bang means it modifies 
+# the receiver in place: that is, it overwrites the input with the output.
 # => "Elementary, my dear Watson!"
 
 "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
