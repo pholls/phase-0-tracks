20e1a2ccfca2c3e089a81d6ce4a3a559fd8ebb0a
Pat Holland <patrick.holland3576@gmail.com>
2016-11-01 11:56:04 -0700

    iterate over oscar_categories hash with .each

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,6 @@
 def my_method
   variable = "\"Hello, World\""
-  puts "The block as not run yet."
+  puts "The block has not run yet."
   yield(variable)
   puts "The block has run."
 end
@@ -10,10 +10,10 @@ my_method { |variable| puts "When a computer wakes up, it says #{variable}" }
 oscar_categories = ["Best Picture", "Best Editing", "Best Director"]
 
 famous_roles = {
-  George_Clooney: "Danny Ocean"
+  George_Clooney: "Danny Ocean",
   Keanu_Reeves: "Neo"
 }
 
-oscar_categories.each do
-  puts oscar_categories
+oscar_categories.each do |category|
+  puts category
 end
