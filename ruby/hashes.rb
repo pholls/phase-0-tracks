e94ad367bf2c5d6dfad4026beaf8734c343ca298
Pat Holland <patrick.holland3576@gmail.com>
2016-10-31 17:23:36 -0700

    pseudocode for client info program

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+# CLIENT INFO PROGRAM 
+# ask for client's name
+# save input in hash
+# ask for client's age
+# save input in hash
+  # convert to integer
+# ask for client's number of children
+  # convert input to integer
+  # save in hash
+# ask for client's retro preference
+  # convert to boolean
+  # save in hash
+# ask for client's decor theme
+  # save input in hash
+#print the hash to the screen
+# ask if user would like to make any updates
+  # convert input to symbol
+  # update value for corresponding key
+  # skip if input is 'none'
+# print hash to the screen
+
