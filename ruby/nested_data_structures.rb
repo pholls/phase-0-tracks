cd7d46c7c14c6a82c68ed988086251a7c69b80b6
Pat Holland <patrick.holland3576@gmail.com>
2016-11-02 13:18:37 -0700

    access items in the nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -13,4 +13,16 @@ school = {
   }
 }
 
-p school
+# p school
+
+p school[:first_floor][:science_classroom]
+
+p school[:second_floor][:math_classroom][1]
+
+p school[:basement][:boiler_room][0]
+
+# Call Array#reverse! on school[:basement][:boiler_room]
+school[:basement][:boiler_room].reverse!
+
+# Print the new result to see how the data has changed
+p school[:basement][:boiler_room][0]
