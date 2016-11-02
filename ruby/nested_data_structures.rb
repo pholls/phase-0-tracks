8d2503a140ab08ac0fbab3441b4258bf710387d8
Pat Holland <patrick.holland3576@gmail.com>
2016-11-02 13:13:46 -0700

    create nested data structure \'school\'

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,16 @@
+school = {
+  basement: {
+    boiler_room: ["Water Heater", "HVAC System"]
+  },
+  first_floor: {
+    gymnasium: ["Basketball Hoop", "Basketball"],
+    science_classroom: ["Beakers", "Bunsen Burners"],
+    spanish_classroom: ["Libro", "Escritorio"]
+  },
+  second_floor: {
+    math_classroom: ["Desk", "Textbook", "Calculator"],
+    english_classroom: ["Poetry", "Novela"]
+  }
+}
+
+p school
