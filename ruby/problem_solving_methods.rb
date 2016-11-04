f1536e4e5a87293ad3cef756fbcec94824f9b376
Pat Holland <patrick.holland3576@gmail.com>
2016-11-04 10:56:15 -0700

    write method to return fibonacci numbers

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -14,6 +14,31 @@ def search_array(array, number)
   b
 end
 
+# Driver Code
 array = [42, 89, 23, 1]
-
 p search_array(array, 9)
+
+# define a method "fib" that takes an integer as an argument
+# initialize an array with [0, 1]
+# create a loop that loops integer times with .times
+# increment the index starting at 0
+  # starting at index + index position + 1
+  # push result into array
+# return an array of the fibonacci numbers run that many times
+
+def fib(number)
+  index = 0
+  array = [0, 1]
+  (number - 2).times do
+    array << array[index] + array[index + 1]
+    index += 1
+  end
+  array
+end
+
+# Driver Code:
+p fib(6)
+p fib(1)
+p fib(100).last == 218922995834555169026
+p fib(0)
+
