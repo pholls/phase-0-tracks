6f7da7567d2ce424ea9828f908691673171a6cac
Pat Holland <patrick.holland3576@gmail.com>
2016-11-07 13:44:28 -0800

    add comments for Puppy class attributes and methods

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# ------- CLASS -------
+# Puppy
+
+# ------- ATTRIBUTES-------
+# Number of legs: 4
+# Number of tails: 1
+# Number of eyes: 2
+# Number of ears: 2
+# Name: varies
+# Ears: floppy
+
+# ------- METHODS -------
+# Play
+# Wag Tail
+# Fetch
+# Poop on the carpet
