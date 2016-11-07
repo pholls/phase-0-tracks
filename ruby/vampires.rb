7dfed176874e3f32618b1cfa34cd63cb3545a0eb
Pat Holland <patrick.holland3576@gmail.com>
2016-11-07 14:11:17 -0800

    refactor insurance and garlic_bread if/then statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -22,20 +22,12 @@ until employee_number.to_i == number do
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
   garlic_bread = gets.chomp
   
-  if garlic_bread.downcase == "yes" || garlic_bread.downcase == "y"
-    garlic_bread = true
-  else
-    garlic_bread = false
-  end
+  garlic_bread = garlic_bread.downcase == "yes" || garlic_bread.downcase == "y"
   
   puts "Would you like to enroll in the company’s health insurance? (y/n)"
   insurance = gets.chomp
   
-  if insurance.downcase == "yes" || insurance.downcase == "y"
-    insurance = true
-  else
-    insurance = false
-  end
+  insurance = insurance.downcase == "yes" || insurance.downcase == "y"
 
   allergy = ''
   until allergy.downcase == 'done'
