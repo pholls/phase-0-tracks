ecdf4e7174e115599031aba64dcb33ab381b3c9f
Pat Holland <patrick.holland3576@gmail.com>
2016-11-08 10:14:27 -0800

    add Puppy#speak and Puppy#roll_over methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,8 +5,22 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    integer.times do |variable|
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
 end
 
 fido = Puppy.new
 
 fido.fetch("ball")
+
+fido.speak(3)
+
+fido.roll_over
