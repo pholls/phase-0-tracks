3d4124b1b3fc87a76a2540bf7e4c90a4fff18578
Pat Holland <patrick.holland3576@gmail.com>
2016-11-08 13:59:00 -0800

    remove switching method, change rec_switching to replace with default values

--- ruby/alias_manager_2.rb
+++ ruby/alias_manager_2.rb
@@ -1,19 +1,11 @@
 # BUSINESS LOGIC
-
-# define a method that takes one argument
-  # a string
-  # and it calls the below method with the string, the vowels, and the consonants
-def switching(string)
-  rec_switching(string, "aeiou", "bcdfghjklmnpqrstvwxyz")
-end
-
 # define a method that takes three arguments
   # a name, a string of vowels, and a string of consonants
   # compare each character to the vowels/consonants
     # if the character is a vowel, put the next vowel into alias_string
     # if the character is a consonant, put the next consonant into alias_string
 # return alias_string
-def rec_switching(name, vowels, consonants)
+def switching(name, vowels = "aeiou", consonants = "bcdfghjklmnpqrstvwxyz")
   new_name = ""
   name.each_char do |letter|
     #this is repetitive code: better way to do this?
@@ -51,17 +43,18 @@ def split_and_switch(alias_array = [], string)
   p user_alias
 end
 
-build_hash("Steve Rogers")
-
 # USER INTERFACE
-my_hash = {}
 
+my_hash = {}
 while true
   puts "Enter your first and last name to receive your Secret Agent Alias.
   Or type 'done'."
   input = gets.chomp
   break if input == 'done'
   my_hash[input] = split_and_switch(input)
+  if input.split(' ').length != 2
+    puts "Invalid input. Try again."
+  end
 end
 
 my_hash.each do |key, value|
