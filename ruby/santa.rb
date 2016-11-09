bfe28b5db30ebfd4b5917da2f73433d352f957a3
Pat Holland <patrick.holland3576@gmail.com>
2016-11-09 15:08:48 -0800

    add attribute changing methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,7 +4,7 @@ class Santa
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
-    reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
 
@@ -16,6 +16,22 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name))
+    @reindeer_ranking << reindeer_name
+  end
+
+  # setter method
+  def change_gender=(new_gender)
+    @gender = new_gender
+  end
+
+
+
 end
 
 santas = []
@@ -25,15 +41,12 @@ example_genders.length.times do |i|
   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
 
-santas.each do
-  p @gender
-end
-
 kris_kringle = Santa.new("gender nonbinary", "hyphenated-American")
 kris_kringle.speak
 kris_kringle.eat_milk_and_cookies("milano")
-p kris_kringle
+kris_kringle
 
 saint_nick = Santa.new("gender fluid", "biracial")
+saint_nick.get_mad_at("Rudolph")
 
 
