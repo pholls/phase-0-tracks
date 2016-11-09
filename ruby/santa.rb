b2cac49a490e7103431abf24183ba38e0374e8bb
Pat Holland <patrick.holland3576@gmail.com>
2016-11-09 13:04:40 -0800

    add eat_milk_and_cookies and initialize methods; test

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,4 +4,18 @@ class Santa
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
 end
+
+saint_nick = Santa.new
+
+saint_nick.speak
+
+saint_nick.eat_milk_and_cookies("Oreo")
