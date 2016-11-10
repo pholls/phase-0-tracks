15572b108f7d63cf26b62a08a060030179e2b58b
Pat Holland <patrick.holland3576@gmail.com>
2016-11-09 17:35:01 -0800

    comment out old code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,14 @@
-module Shout
+# module Shout
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yell_happily(words)
-    words.upcase + "!!!" + " XD"
-  end
-end
+#   def self.yell_happily(words)
+#     words.upcase + "!!!" + " XD"
+#   end
+# end
 
-p Shout.yell_angrily("EVERYTHING IS BROKEN")
-p Shout.yell_happily("everything is going to be okay")
+# p Shout.yell_angrily("EVERYTHING IS BROKEN")
+# p Shout.yell_happily("everything is going to be okay")
+
