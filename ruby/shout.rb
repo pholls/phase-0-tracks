5da7b3ca6f30e51e1b1eccadbd62f15f241a9229
Pat Holland <patrick.holland3576@gmail.com>
2016-11-09 17:49:03 -0800

    rewrite Shout module, write two classes, call module methods on an instance of each

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -12,3 +12,27 @@
 # p Shout.yell_angrily("EVERYTHING IS BROKEN")
 # p Shout.yell_happily("everything is going to be okay")
 
+module Shout
+  def yell_angrily(words)
+    puts words.upcase + "!!! >:("
+  end
+  def yell_happily(words)
+    puts words.upcase + "! XD XD XD"
+  end
+end
+
+class Child
+  include Shout
+end
+
+class Angry_Old_Man
+  include Shout
+end
+
+toddler = Child.new
+toddler.yell_happily("I love ice cream!")
+toddler.yell_angrily("I hate raisins!")
+
+old_guy = Angry_Old_Man.new
+old_guy.yell_angrily("The rent is 2 damn high!")
+old_guy.yell_happily("Ya gotta believe")
