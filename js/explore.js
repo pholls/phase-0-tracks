4a6eac70d6fe518b3f989b427e6cfce95bfbb81c
Pat Holland <patrick.holland3576@gmail.com>
2016-11-16 19:37:15 -0800

    define function to reverse string

--- js/explore.js
+++ js/explore.js
@@ -4,6 +4,13 @@
   // do this for each character
   // return the new string
 
+
 function reverse(string) {
-  
+  var newString = "";
+  for (var i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
 }
+
+console.log(reverse("hello"));
