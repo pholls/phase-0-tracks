6c92ae8dbca51de7ec08ec5d1c4b8050ec82de30
Pat Holland <patrick.holland3576@gmail.com>
2016-11-16 16:39:47 -0800

    pseudocode reverse function

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// function will take a string as an argument
+// look at the last character of the string
+  // add that to a new string, then look at second-to-last character
+  // do this for each character
+  // return the new string
+
+function reverse(string) {
+  
+}
