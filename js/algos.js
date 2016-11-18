7b0d6a0dd1c4e9d9c13643d8c61ebcec6d30e3a2
Pat Holland <patrick.holland3576@gmail.com>
2016-11-18 15:00:48 -0800

    release 0 pseudocode

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// function that takes array of strings and returns the longest one
+// loop over the array
+// var longestWord == ''
+// if index == 0, var previousLength == 0
+// otherwise, previousLength == currentLength
+// call .length on current index
+// update currentLength
+// if currentLength > previousLength, update longestWord to the current String
+// otherwise, skip that step
+// return longestWord
