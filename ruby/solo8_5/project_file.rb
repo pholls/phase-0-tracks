2abc94c0e14ce1dbebbef942f0952526ad2723fd
Pat Holland <patrick.holland3576@gmail.com>
2016-11-26 09:29:26 -0800

    start pseudocode

---
+++ ruby/solo8_5/project_file.rb
@@ -0,0 +1,16 @@
+# BUSINESS LOGIC
+
+# Patient Database
+# Fields: ID (primary key), first_name, last_name, insurance (boolean), 
+# next appointment (date), last appointment (date)
+
+# Program abilities:
+# Add a new patient
+# Delete a patient by name or by ID number
+# View a patient's last appointment
+# View a patient's upcoming appointment
+# View patient's insurance status
+# Change a patient's upcoming appointment
+# Update patient's insurance status
+
+
