c7952613c205cd0c43202d6718e60301a9bf4d26
Pat Holland <patrick.holland3576@gmail.com>
2016-11-26 09:40:52 -0800

    continue pseudocode

--- ruby/solo8_5/project_file.rb
+++ ruby/solo8_5/project_file.rb
@@ -1,8 +1,19 @@
 # BUSINESS LOGIC
 
+# Require gems:
+# Sqlite3
+# Faker
+
+#Create (if not exist) databases:
+# Doctors
+# Patients
+
+# Doctor's Database
+# ID (primary key), name, specialization
+
 # Patient Database
-# Fields: ID (primary key), first_name, last_name, insurance (boolean), 
-# next appointment (date), last appointment (date)
+# Fields: ID (primary key), name, insurance (boolean), 
+# next appointment (date), last appointment (date), doctor (foreign key)
 
 # Program abilities:
 # Add a new patient
@@ -10,7 +21,10 @@
 # View a patient's last appointment
 # View a patient's upcoming appointment
 # View patient's insurance status
+# View patient's doctor's name
 # Change a patient's upcoming appointment
 # Update patient's insurance status
+# Add/remove doctors
+# Change specialization by doctor's name or id
 
 
