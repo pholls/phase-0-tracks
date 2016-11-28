7312455c93c199fcc10526234cc2b94195b70b18
Pat Holland <patrick.holland3576@gmail.com>
2016-11-28 12:15:11 -0800

    refactor view_patient_list method

--- ruby/solo8_5/project_file.rb
+++ ruby/solo8_5/project_file.rb
@@ -153,15 +153,12 @@ def change_spec(db, name_or_id, new_spec)
 end
 # View all of a doctor's patients
 def view_patient_list(db, doctor_name_or_id)
-  if doctor_name_or_id.to_i >= 1
-    patients = db.execute("SELECT patients.name FROM patients JOIN doctors ON patients.doctor_id=doctors.id WHERE doctors.id=?", [doctor_name_or_id])
-  else
-    patients = db.execute("SELECT patients.name FROM patients JOIN doctors ON patients.doctor_id=doctors.id WHERE doctors.name=?", [doctor_name_or_id])
-  end
+  patients = db.execute("SELECT patients.name FROM patients JOIN doctors ON patients.doctor_id=doctors.id WHERE doctors.id=? OR doctors.name=?", [doctor_name_or_id, doctor_name_or_id])
   patients.each do |hash|
     p hash['name']
   end
 end
+
 # View all of a doctor's upcoming appointments
 def view_doctor_appointments(db, doctor_name_or_id)
   appointments = db.execute("SELECT patients.name, patients.next_appt FROM patients JOIN doctors ON patients.doctor_id=doctors.id WHERE doctors.id=? OR doctors.name=?", [doctor_name_or_id, doctor_name_or_id])
