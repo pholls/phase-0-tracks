db88e03cf385548d7bb2711a79e2b10340aafa9a
Pat Holland <patrick.holland3576@gmail.com>
2016-12-01 15:03:25 -0800

    finish playing with jquery functions

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,10 +1,23 @@
-$(document).ready(function(){
-  $('img').click(function(){
+$(document).ready(function() {
+
+  $('#header').after('<p><em>This is new text added in jQuery!</em></p>');
+
+  $("#list").after('<p><em>Click to hide list items</em></p>');
+
+  $('img').click(function() {
     $(this).fadeToggle('slow');
   });
-  $('#header').after('<p><em>New text!</em></p>');
-  $("#list").after('<p><em>Click to hide list items</em></p>');
+
   $('ul li').click(function(){
     $(this).slideToggle();
   });
+
+  $('#header').click(function() {
+    $(this).css({'color': 'red'});
+  });
+
+  $('p:not(:first)').click(function() {
+    $(this).css({'color': 'blue'});
+  });
+
 });
