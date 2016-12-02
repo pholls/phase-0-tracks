97a0ce419223ee410180bcb238f8f3573524b5a5
Pat Holland <patrick.holland3576@gmail.com>
2016-12-01 17:01:57 -0800

    write GET that displays an address

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,23 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET that displays an address
+
+get '/contact' do
+  "1600 Pennsylvania Avenue NW<br>Washington, DC 20500, USA"
+end
+
+# write a GET that takes a person's name as a query parameter
+# (not a route parameter) and says "Good job, [name]!"
+# If query parameter is missing, route just says "Good job!"
+
+
+# A route that uses route parameters to add 
+# two numbers and respond with the result
+
+
+#BONUS: route that allows user to search the database somehow
+# (you can simply modify the home page to take a query parameter
+# and filter the students displayed if a query parameter is present)
+
